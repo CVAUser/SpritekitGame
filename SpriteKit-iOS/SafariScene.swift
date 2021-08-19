71ab38c04ad5de941314841347db6b0fea8a518f
CVAUser <57252726+CVAUser@users.noreply.github.com>
2021-08-19 21:37:59 +0300

    add safari scene file

---
+++ SpriteKit-iOS/SafariScene.swift
@@ -0,0 +1,16 @@
+//
+//  SafariScene.swift
+//  SpriteKit-iOS
+//
+//  Created by Slava on 19.08.2021.
+//  Copyright © 2021 Slava. All rights reserved.
+//
+
+import SpriteKit
+
+class SafariScene: SKScene {
+    
+    override func didMove(to view: SKView) {
+        <#code#>
+    }
+}
